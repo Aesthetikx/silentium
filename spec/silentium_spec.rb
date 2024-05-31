dd5e9355845687045b11d4f6ddef96e3633fe7b8
John DeSilva <desilvjo@umich.edu>
2024-05-31 18:53:11 -0400

    Remove placeholder spec

--- spec/silentium_spec.rb
+++ spec/silentium_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Silentium do
   it "has a version number" do
     expect(Silentium::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
